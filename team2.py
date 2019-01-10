d806eaacd75fe2b165850fca3dc63fd5ae11cdd5
myisaac1127 <isaac.soles@acecharterhigh.org>
2019-01-10 08:52:19 -0800

    Stratagy change

--- team2.py
+++ team2.py
@@ -7,8 +7,15 @@
 ####
 
 team_name = 'CHEESE MEASE' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Collude'
+strategy_description = '''Make my move based on the history with this player.
+    
+    history: a string with one letter (c or b) per round that has been played with this opponent.
+    their_history: a string of the same length as history, possibly empty. 
+    The first round between these two players is my_history[0] and their_history[0]
+    The most recent round is my_history[-1] and their_history[-1]
+    
+    Returns 'c' or 'b' for collude or betray..'''
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
