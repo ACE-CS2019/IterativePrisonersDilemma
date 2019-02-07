d62dc13a567c1d6299426fb46c1111c8b43e2526
rosanavarro <rosa.navarro@acecharterhigh.org>
2019-02-07 08:43:11 -0800

    Lady Sharks strategy
    If you betray, then we might betray or collude.

--- team5.py
+++ team5.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Lady Sharks' # Only 10 chars displayed.
+strategy_name = 'You think we like you?!'
+strategy_description = 'If you collude, we might deny knowing you.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,7 +26,10 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if 'c' in their_history or len(their_history)>50: 
+        return 'b'               
+    else:
+        return 'c'         
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
